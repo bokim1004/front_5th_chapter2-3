5528738db3739a3f501ff74f05b1f2578ee63a85
chloekim <chloe@perfitt.io>
2025-04-29 11:06:21 +0900

    refactor(features/comment/ui):댓글렌더링부분 컴포넌트로 분리

--- src/features/post/ui/PostDetailDialog.tsx
+++ src/features/post/ui/PostDetailDialog.tsx
@@ -1,10 +1,11 @@
+import CommentList from "@/features/comment/ui/CommentList"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/shared/ui/dialog"
 
 {
   /* 게시물 상세 보기 대화상자 */
 }
 function PostDetailDialog() {
-  //highlightText,renderComments,selectedPost,showPostDetailDialog,setShowPostDetailDialog,searchQuery값 필요
+  //highlightText,selectedPost,showPostDetailDialog,setShowPostDetailDialog,searchQuery값 필요
   return (
     <Dialog open={showPostDetailDialog} onOpenChange={setShowPostDetailDialog}>
       <DialogContent className="max-w-3xl">
@@ -13,7 +14,7 @@ function PostDetailDialog() {
         </DialogHeader>
         <div className="space-y-4">
           <p>{highlightText(selectedPost?.body, searchQuery)}</p>
-          {renderComments(selectedPost?.id)}
+          {CommentList(selectedPost?.id)}
         </div>
       </DialogContent>
     </Dialog>
